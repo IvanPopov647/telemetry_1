3019923ac769b0457794b15ae9be7b8b4714846a
Acer <popovvancik70@mail.ru>
2022-03-10 22:30:20 +0300

    Задача 2 выполнена

---
+++ work_2/work_2.py
@@ -0,0 +1,20 @@
+my_voc={ 1 :'value', 'printer': 3, 'price':400}
+my_voc_2={ 5: 100, 'gruppa':12, 'printer': 3, 'price':400}
+my_voc["xerox"]=55
+my_voc["printer"]=200
+
+my_voc.update(my_voc_2)
+
+print(my_voc)
+print(my_voc["printer"])
+
+#massive = [1, 2, 3, 4, 10]
+
+#my_set={1, 3, 5, 6, 8}
+#my_set2=set(massive)
+#my_set3=my_set - my_set2
+
+#my_set.add('my_set2')
+#print(my_set)
+
+
